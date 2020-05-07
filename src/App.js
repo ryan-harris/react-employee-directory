1b3599b00670dc15f0b0fb9f344d065baf1cb881
Ryan Harris <harris.ucla@gmail.com>
2020-05-06 21:20:41 -0700

    Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      {/* <DirectoryTable /> */}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Employee Directory</h1>
+      <p>Click on a column heading to sort or narrow results with the search</p>
+    </header>
+  );
+};
+
+export default Header;
